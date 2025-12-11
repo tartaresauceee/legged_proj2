e157ce20d0e9f1d4f5d6b8cf9def8b1dcd5d9491
Victor Dubois <victor.dubois@outlook.com>
2025-12-11 16:27:46 +0100

    plot velocity

--- load_sb3.py
+++ load_sb3.py
@@ -57,7 +57,7 @@ from utils.file_utils import get_latest_model, load_all_results
 LEARNING_ALG = "PPO" #"SAC"
 interm_dir = "./logs/intermediate_models/"
 # path to saved models, i.e. interm_dir + '102824115106'
-log_dir = interm_dir + '120325180443'
+log_dir = interm_dir + '120825232703'
 
 # initialize env configs (render at test time)
 # check ideal conditions, as well as robustness to UNSEEN noise during training
@@ -65,7 +65,7 @@ env_config = {"motor_control_mode":"CPG", #"CARTESIAN_PD"
                "task_env": "LR_COURSE_TASK", #  "LR_COURSE_TASK", "FWD_LOCOMOTION"
                "observation_space_mode": "LR_COURSE_OBS",
                "terrain": None}
-env_config['render'] = True
+env_config['render'] = False
 env_config['record_video'] = False
 env_config['add_noise'] = False 
 
@@ -95,7 +95,7 @@ obs = env.reset()
 episode_reward = 0
 
 # [TODO] initialize arrays to save data from simulation 
-
+vel = np.zeros(2000)
 for i in range(2000):
     action, _states = model.predict(obs,deterministic=False) # sample at test time? ([TODO]: test if the outputs make sense)
     obs, rewards, dones, info = env.step(action)
@@ -108,5 +108,11 @@ for i in range(2000):
 
     # [TODO] save data from current robot states for plots 
     # To get base position, for example: env.envs[0].env.robot.GetBasePosition() 
+    vel[i] = env.envs[0].env.robot.GetBaseLinearVelocity()[0]
     
 # [TODO] make plots
+figure = plt.figure()
+
+plt.plot(range(2000), vel)
+
+plt.show()
